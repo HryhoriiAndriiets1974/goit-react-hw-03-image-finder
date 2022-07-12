98fa6066c906ebfcbbb3f5cd6431de6a740046f9
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-07-12 23:32:04 +0300

    axios-imagesApi

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,15 @@
+import {BallTriangle} from "react-loader-spinner";
+
+const Loader = () => {
+  return (
+    <section>
+      <BallTriangle
+        color="#00BFFF"
+        height={300}
+        width={300}
+      />
+    </section>
+  )
+};
+
+export default Loader;
